318796c8dde487d6ed6d018a6d97e37d1693c77c
David Reyna Barreto <reyna_b@upeu.du.pe>
2015-09-29 00:54:56 -0500

    v3

--- src/java/controllers/UsuarioControllers.java
+++ src/java/controllers/UsuarioControllers.java
@@ -3,25 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package validar;
+package controllers;
 
-import daos.UsuarioDAO;
-import interfaces.UsuarioInterface;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import modelos.Usuario;
 
 /**
  *
  * @author Docente
  */
-public class Autentificacion extends HttpServlet {
+public class UsuarioControllers extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -40,10 +35,10 @@ public class Autentificacion extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Autentificacion</title>");            
+            out.println("<title>Servlet UsuarioControllers</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Autentificacion at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet UsuarioControllers at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -61,14 +56,7 @@ public class Autentificacion extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        out.println("salir");
-   
-        HttpSession session = request.getSession();       
-            session.invalidate(); String url = "index.html";
-            RequestDispatcher dispatcher = request.getRequestDispatcher("index.html");
-            dispatcher.forward(request, response);        
+        processRequest(request, response);
     }
 
     /**
@@ -82,26 +70,7 @@ public class Autentificacion extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
-        int a; Usuario u1 = new Usuario();
-        HttpSession session = request.getSession(true);
-        //validarUsuario
-             UsuarioInterface ui = new UsuarioDAO();
-        Usuario users = new Usuario();
-        String user, pass;
-        user = request.getParameter("usuario");
-        pass = request.getParameter("clave");
-        users = ui.validarUsuario(user, pass);
-        a = ui.validauser(user, pass);
-        
-        if(a!=0){
-            u1 = ui.listarUser(a);
-            session.setAttribute("userid", u1.getId());  
-            session.setAttribute("user", u1.getUser());  
-            response.sendRedirect("main");         
-        }else{
-            response.sendRedirect("error");
-        }
+        processRequest(request, response);
     }
 
     /**
